059f525cb714fb89f9b88c1a4fccbab3c3e8a785
Gopal Gupta <gopaljilab@gmail.com>
2025-10-18 04:56:06 +0530

    test: add and verify MaxFrequencyElements solution in array package

--- src/main/java/gopaljilab/dsa/array/MaxFrequencyElements.java
+++ src/main/java/gopaljilab/dsa/array/MaxFrequencyElements.java
@@ -1,4 +1,37 @@
 package gopaljilab.dsa.array;
 
 public class MaxFrequencyElements {
+    public int maxFrequencyElements(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        if (nums.length == 1)
+            return 1;
+
+        // Find maximum value
+        int max = nums[0];
+        for (int num : nums)
+            if (num > max)
+                max = num;
+
+        // Create and fill frequency array
+        int[] countArray = new int[max+1];
+        for (int num : nums){
+            countArray[num]++;
+        }
+
+        int tempMaxFrequency = 0;
+        int maxFrequency = 0;
+        for (int i = 0; i < countArray.length; i++) {
+            if (countArray[i] > tempMaxFrequency) {
+                tempMaxFrequency = countArray[i];
+            }
+        }
+        for (int i = 0; i < countArray.length; i++) {
+            if (tempMaxFrequency == countArray[i]){
+                maxFrequency += countArray[i];
+            }
+        }
+        return maxFrequency;
+    }
 }
