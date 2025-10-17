879f6b70d63c9948aa87a6bdcaf818f935976085
Gopal Gupta <gopaljilab@gmail.com>
2025-10-18 04:57:08 +0530

    test: add and verify test cases for MaxFrequencyElements solution

--- src/test/java/array/MaxFrequencyElementsTest.java
+++ src/test/java/array/MaxFrequencyElementsTest.java
@@ -1,4 +1,71 @@
 package array;
 
+import gopaljilab.dsa.array.MaxFrequencyElements;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MaxFrequencyElementsTest {
+
+
+
+    @Test
+    public void TestBaseCase(){
+        MaxFrequencyElements maxFrequencyElements=
+                new MaxFrequencyElements();
+        int[] nums = {1,2,2,3,1,4};
+        int expectedResult = 4;
+        int actualResult =
+                maxFrequencyElements.maxFrequencyElements(nums);
+        assertEquals(expectedResult, actualResult,
+                "MaxFrequencyElements of [1,2,2,3,1,4] should be 4");
+    }
+
+    @Test
+    public void TestAllElementsHaveSameFrequencyCase(){
+        MaxFrequencyElements maxFrequencyElements=
+                new MaxFrequencyElements();
+        int[] nums = {1,2,3,4,5};
+        int expectedResult = 5;
+        int actualResult =
+                maxFrequencyElements.maxFrequencyElements(nums);
+        assertEquals(expectedResult, actualResult,
+                "MaxFrequencyElements of [1,2,3,4,5] should be 5");
+    }
+
+    @Test
+    public void TestNoElementsCase(){
+        MaxFrequencyElements maxFrequencyElements=
+                new MaxFrequencyElements();
+        int[] nums = {};
+        int expectedResult = 0;
+        int actualResult =
+                maxFrequencyElements.maxFrequencyElements(nums);
+        assertEquals(expectedResult, actualResult,
+                "MaxFrequencyElements of [] should be 0");
+    }
+
+    @Test
+    public void TestOneElementCase(){
+        MaxFrequencyElements maxFrequencyElements=
+                new MaxFrequencyElements();
+        int[] nums = {5};
+        int expectedResult = 1;
+        int actualResult =
+                maxFrequencyElements.maxFrequencyElements(nums);
+        assertEquals(expectedResult, actualResult,
+                "MaxFrequencyElements of [5] should be 1");
+    }
+
+    @Test
+    public void TestAllElementsAreSameCase(){
+        MaxFrequencyElements maxFrequencyElements=
+                new MaxFrequencyElements();
+        int[] nums = {1,1,1,1};
+        int expectedResult = 4;
+        int actualResult =
+                maxFrequencyElements.maxFrequencyElements(nums);
+        assertEquals(expectedResult, actualResult,
+                "MaxFrequencyElements of [1,1,1,1] should be 4");
+    }
 }
