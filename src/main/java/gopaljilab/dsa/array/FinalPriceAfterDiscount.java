8aa2719bd5d22b40dbd1fe1ec6b6459cc0c7500a
Gopal Gupta <gopaljilab@gmail.com>
2025-11-19 02:16:24 +0530

    test: add and verify Final Price After Discount solution in array package

---
+++ src/main/java/gopaljilab/dsa/array/FinalPriceAfterDiscount.java
@@ -0,0 +1,24 @@
+package gopaljilab.dsa.array;
+
+public class FinalPriceAfterDiscount {
+    public int[] finalPrices(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0; i<prices.length; i++){
+            int finalPrice = prices[i];  // Start with original price
+
+            // Look for first j > i where prices[j] <= prices[i]
+            for (int j = i+1; j < prices.length; j++){
+                if (prices[j] <= prices[i]) {
+                    finalPrice = prices[i] - prices[j];
+                    break;
+                }
+            }
+
+            // If no discount found, price remains the same
+            answer[i] = finalPrice;
+        }
+
+        return answer;
+    }
+}
