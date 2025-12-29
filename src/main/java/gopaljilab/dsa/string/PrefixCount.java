48c94c616494ce704798dd2deaa0134766a8352b
Gopal Gupta <gopaljilab@gmail.com>
2025-12-30 03:15:57 +0530

    test : test and verify solution for Counting words with in a given prefix in string package

---
+++ src/main/java/gopaljilab/dsa/string/PrefixCount.java
@@ -0,0 +1,12 @@
+package gopaljilab.dsa.string;
+
+public class PrefixCount {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(pref))
+                count++;
+        }
+        return count;
+    }
+}
