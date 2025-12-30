6339a63438f1a5eee5ab14fe2c8be99519538ed5
Gopal Gupta <gopaljilab@gmail.com>
2025-12-31 00:01:25 +0530

    test : test and verify test cases for Mirror Distance solution

---
+++ src/test/java/math/MirrorDistanceTest.java
@@ -0,0 +1,15 @@
+package math;
+
+import gopaljilab.dsa.array.MinimumAverage;
+
+public class MirrorDistanceTest {
+        public static void main(String[] args) {
+            System.out.println(MinimumAverage.minimumAverage(new int[]{1,2,3,4}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{7,1,5,3}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{-10,-5,0,5}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{4,4,4,4}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{100000,1,99999,2}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{8,2}));
+            System.out.println(MinimumAverage.minimumAverage(new int[]{1,2,3}));
+        }
+}
