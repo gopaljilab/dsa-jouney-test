af970d4a2fb9ad6afa75467ef874ae039aa2e274
Gopal Gupta <gopaljilab@gmail.com>
2026-03-04 23:56:32 +0530

    test: verify the solution for Remove Duplicates In Sorted Array

---
+++ src/main/java/gopaljilab/dsa/array/RemoveDuplicatesInSortedArray.java
@@ -0,0 +1,14 @@
+package gopaljilab.dsa.array;
+
+public class RemoveDuplicatesInSortedArray {
+    public static int removeDuplicates(int[] nums){
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
