9da1ed9aa9508e1bf217f8d81e4524f70e1a18ad
Gopal Gupta <gopaljilab@gmail.com>
2026-03-21 23:56:23 +0530

    test: verify the Count And Say solution

---
+++ src/main/java/gopaljilab/dsa/string/CountAndSay.java
@@ -0,0 +1,29 @@
+package gopaljilab.dsa.string;
+
+public class CountAndSay {
+    public String countAndSay(int n) {
+        StringBuilder result = new StringBuilder();
+        result.append("1");
+        if (n==1) return result.toString();
+
+        for (int i = 2; i < n; i++) {
+            StringBuilder newString = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < result.length(); j++) {
+
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    newString.append(count);
+                    newString.append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            // Add last group
+            newString.append(count);
+            newString.append(result.charAt(result.length()-1));
+            result = newString;
+        }
+        return result.toString();
+    }
+}
