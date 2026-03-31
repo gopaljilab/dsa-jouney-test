382a1050574c9c28fe9207376659642c81403fe4
Gopal Gupta <gopaljilab@gmail.com>
2026-03-31 23:59:21 +0530

    Refactor: Max Distinct Sub String solution

--- src/main/java/gopaljilab/dsa/string/MaxDistinctSubString.java
+++ src/main/java/gopaljilab/dsa/string/MaxDistinctSubString.java
@@ -2,14 +2,15 @@ package gopaljilab.dsa.string;
 
 public class MaxDistinctSubString {
     public static int maxDistinct(String s) {
-        int count = 0;
-        StringBuilder subString = new StringBuilder();
-        for(int i=0; i<s.length()-1; i++){
-            if(s.charAt(i) != s.charAt(i+1)){
+        if(s == null || s.isEmpty()) return 0;
+
+        int count = 1;
+
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) != s.charAt(i - 1)){
                 count++;
             }
-            subString.append(s.charAt(i));
         }
-        return count+1;
+        return count;
     }
 }
